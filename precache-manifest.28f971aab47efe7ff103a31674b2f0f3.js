a4c7aa7bed9a44c1178a769de16331f5ade8695a
wazeem123 <mwazeem84@gmail.com>
2020-04-04 19:26:11 +0530

    Updates

---
+++ precache-manifest.28f971aab47efe7ff103a31674b2f0f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09529c2fdd600f7ddb368247de8021c5",
+    "url": "/Peoplefirst/index.html"
+  },
+  {
+    "revision": "be4d95be70e56c212717",
+    "url": "/Peoplefirst/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ecefe27a362454b0f01b",
+    "url": "/Peoplefirst/static/js/2.c18e9c97.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Peoplefirst/static/js/2.c18e9c97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4d95be70e56c212717",
+    "url": "/Peoplefirst/static/js/main.1afbd849.chunk.js"
+  },
+  {
+    "revision": "74529caabb8679c63cb0",
+    "url": "/Peoplefirst/static/js/runtime-main.915f381f.js"
+  },
+  {
+    "revision": "d32fe79fa0818d7aa15aa76dad12df2c",
+    "url": "/Peoplefirst/static/media/micBtn.d32fe79f.png"
+  }
+]);
